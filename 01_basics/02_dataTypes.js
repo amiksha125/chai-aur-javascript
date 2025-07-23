223f400f02fcc4ff7846e9344a64278d874fe372
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-23 14:31:30 +0000

    basics of JS datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" //treats all JS code as newer version
+
+//alert(3 + 3) //we are using node js not browser
+console.log(3 
+    +
+    3
+); //Avoid! code readability should be high
+
+let name = "Amiksha";
+let age = 20;
+let isLoggedIn = true;
+let state = null;
+
+//number => 2 ^ 52
+//bigint  (for trading and banking related projects to stoe big values)
+//string => ""
+//boolean => true/ false
+//null => standalone value (representation of empty value)
+//undefined => assigned to declared but not defined variables
+//symbol => to find unquiness of components
+
+//objects
+
+console.log(typeof state); //object
+console.log(typeof undefined)
+
